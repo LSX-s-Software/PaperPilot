162ee8021a8016fa9d80f8b9a01f084aa90f71b7
Nagico <yjr888@vip.qq.com>
2023-10-01 00:07:30 +0800

    feat(python): 添加context类型

--- paperpilot-common-python/paperpilot_common/utils/types.py
+++ paperpilot-common-python/paperpilot_common/utils/types.py
@@ -1,4 +1,21 @@
+import abc
+import collections.abc
+import typing
 from typing import Dict, List, Union
+
+import grpc
+import grpc.aio
+
+_T = typing.TypeVar("_T")
+
+
+class _MaybeAsyncIterator(collections.abc.AsyncIterator[_T], collections.abc.Iterator[_T], metaclass=abc.ABCMeta):
+    pass
+
+
+class _ServicerContext(grpc.ServicerContext, grpc.aio.ServicerContext):
+    pass
+
 
 SimpleValue = Union[str, int, bool, float]
 
