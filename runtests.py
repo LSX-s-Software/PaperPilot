8c7d5a67dc76caec34020e14152902bff5c0d438
Nagico <yjr888@vip.qq.com>
2023-10-04 15:12:11 +0800

    feat: 实现测试接口

---
+++ runtests.py
@@ -0,0 +1,108 @@
+#! /usr/bin/env python3
+import os
+import sys
+
+import pytest
+
+
+def split_class_and_function(string):
+    class_string, function_string = string.split(".", 1)
+    return "%s and %s" % (class_string, function_string)
+
+
+def is_function(string):
+    # `True` if it looks like a test function is included in the string.
+    return string.startswith("test_") or ".test_" in string
+
+
+def is_class(string):
+    # `True` if first character is uppercase - assume it's a class name.
+    return string[0] == string[0].upper()
+
+
+class PytestTestRunner:
+    """Runs pytest to discover and run tests."""
+
+    def __init__(self, verbosity=1, failfast=False, keepdb=False, **kwargs):
+        self.verbosity = verbosity
+        self.failfast = failfast
+        self.keepdb = keepdb
+
+    @classmethod
+    def add_arguments(cls, parser):
+        parser.add_argument(
+            "--keepdb",
+            action="store_true",
+            help="Preserves the test DB between runs.",
+        )
+
+    def run_tests(self, test_labels):
+        """Run pytest and return the exitcode.
+
+        It translates some of Django's test command option to pytest's.
+        """
+        import pytest
+
+        argv = []
+        if self.verbosity == 0:
+            argv.append("--quiet")
+        if self.verbosity == 2:
+            argv.append("--verbose")
+        if self.verbosity == 3:
+            argv.append("-vv")
+        if self.failfast:
+            argv.append("--exitfirst")
+        if self.keepdb:
+            argv.append("--reuse-db")
+
+        argv.extend(test_labels)
+        return pytest.main(argv)
+
+
+if __name__ == "__main__":
+    os.environ["DJANGO_ENV"] = "test"
+    os.environ["DJANGO_SECRET_KEY"] = "secret"
+    os.environ["DJANGO_SETTINGS_MODULE"] = "server.settings"
+
+    os.environ["CACHE_URL"] = "locmem://"
+
+    if len(sys.argv) > 1:
+        pytest_args = sys.argv[1:]
+        first_arg = pytest_args[0]
+
+        if "--local" in pytest_args:
+            pytest_args.remove("--local")
+            pytest_args = [
+                "--cov",
+                "server",
+                "--cov-report",
+                "xml",
+                "--cov-report",
+                "term",
+                "--cov-report",
+                "html:htmlcov",
+            ] + pytest_args
+        elif "--coverage" in pytest_args:
+            pytest_args.remove("--coverage")
+            pytest_args = [
+                "--cov",
+                "server",
+                "--cov-report",
+                "xml",
+            ] + pytest_args
+
+        if first_arg.startswith("-"):
+            # `runtests.py [flags]`
+            pytest_args = ["tests"] + pytest_args
+        elif is_class(first_arg) and is_function(first_arg):
+            # `runtests.py TestCase.test_function [flags]`
+            expression = split_class_and_function(first_arg)
+            pytest_args = ["tests", "-k", expression] + pytest_args[1:]
+        elif is_class(first_arg) or is_function(first_arg):
+            # `runtests.py TestCase [flags]`
+            # `runtests.py test_function [flags]`
+            pytest_args = ["tests", "-k", pytest_args[0]] + pytest_args[1:]
+    else:
+        pytest_args = []
+
+    sys.exit(pytest.main(pytest_args))
---
+++ server/utils/logging_handler.py
@@ -0,0 +1,115 @@
+# 先声明一个类继承logging.Handler
+import logging
+import os
+import sys
+
+from loguru import logger
+
+
+class InterceptTimedRotatingFileHandler(logging.Handler):
+    """
+    自定义反射时间回滚日志记录器
+    缺少命名空间
+    """
+
+    def __init__(
+        self,
+        filename,
+        when="d",
+        interval=1,
+        backupCount=15,
+        encoding="utf-8",
+        delay=False,
+        utc=False,
+        atTime=None,
+        logging_levels="all",
+    ):
+        super(InterceptTimedRotatingFileHandler, self).__init__()
+        self.atTime = atTime
+        filename = os.path.abspath(filename)
+        when = when.lower()
+        # 需要本地用不同的文件名做为不同日志的筛选器
+        self.logger_ = logger.bind(sime=filename)
+        self.filename = filename
+        key_map = {
+            "h": "hour",
+            "w": "week",
+            "s": "second",
+            "m": "minute",
+            "d": "day",
+        }
+        # 根据输入文件格式及时间回滚设立文件名称
+        rotation = "%d %s" % (interval, key_map[when])
+        retention = "%d %ss" % (backupCount, key_map[when])
+        time_format = "{time:%Y-%m-%d_%H-%M-%S}"
+        if when == "s":
+            time_format = "{time:%Y-%m-%d_%H-%M-%S}"
+        elif when == "m":
+            time_format = "{time:%Y-%m-%d_%H-%M}"
+        elif when == "h":
+            time_format = "{time:%Y-%m-%d_%H}"
+        elif when == "d":
+            time_format = "{time:%Y-%m-%d}"
+        elif when == "w":
+            time_format = "{time:%Y-%m-%d}"
+        level_keys = ["info"]
+        # 构建一个筛选器
+        levels = {
+            "debug": lambda x: "DEBUG" == x["level"].name.upper()
+            and x["extra"].get("sime") == filename,
+            "error": lambda x: "ERROR" == x["level"].name.upper()
+            and x["extra"].get("sime") == filename,
+            "info": lambda x: "INFO" == x["level"].name.upper()
+            and x["extra"].get("sime") == filename,
+            "warning": lambda x: "WARNING" == x["level"].name.upper()
+            and x["extra"].get("sime") == filename,
+        }
+        # 根据输出构建筛选器
+        if isinstance(logging_levels, str):
+            if logging_levels.lower() == "all":
+                level_keys = levels.keys()
+            elif logging_levels.lower() in levels:
+                level_keys = [logging_levels]
+        elif isinstance(logging_levels, (list, tuple)):
+            level_keys = logging_levels
+        for k, f in {_: levels[_] for _ in level_keys}.items():
+            # 为防止重复添加sink，而重复写入日志，需要判断是否已经装载了对应sink，防止其使用秘技：反复横跳。
+            filename_fmt = filename.replace(
+                ".log", "_%s_%s.log" % (time_format, k)
+            )
+            # noinspection PyUnresolvedReferences,PyProtectedMember
+            file_key = {
+                _._name: han_id
+                for han_id, _ in self.logger_._core.handlers.items()
+            }
+            filename_fmt_key = "'{}'".format(filename_fmt)
+            if filename_fmt_key in file_key:
+                continue
+                # self.logger_.remove(file_key[filename_fmt_key])
+            self.logger_.add(
+                filename_fmt,
+                retention=retention,
+                encoding=encoding,
+                level=self.level,
+                rotation=rotation,
+                compression="tar.gz",  # 日志归档自行压缩文件
+                delay=delay,
+                enqueue=True,
+                filter=f,
+            )
+
+    def emit(self, record):
+        named_logger = self.logger_.bind(title=record.name)
+        try:
+            level = named_logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        frame, depth = logging.currentframe(), 2
+        # 把当前帧的栈深度回到发生异常的堆栈深度，不然就是当前帧发生异常而无法回溯
+        while frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+        named_logger.opt(depth=depth, exception=record.exc_info).log(
+            level, record.getMessage()
+        )
