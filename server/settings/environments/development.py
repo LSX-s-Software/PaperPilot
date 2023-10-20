157356114c6ead1caa9be58411f890c4a0c17db9
Nagico <yjr888@vip.qq.com>
2023-10-20 15:57:57 +0800

    fix: 修复格式

--- server/settings/environments/development.py
+++ server/settings/environments/development.py
@@ -4,10 +4,6 @@ This file contains all the settings that defines the development server.
 SECURITY WARNING: don't run with debug turned on in production!
 """
 
-import socket
-
-from loguru import logger
-
 from server.settings.util import config
 
 # Setting the development status:
--- tests/apps/conftest.py
+++ tests/apps/conftest.py
@@ -1,12 +1,7 @@
-import uuid
 from typing import Tuple
 from uuid import UUID
 
 import pytest
-import pytest_asyncio
-from django.utils import timezone
-from google.protobuf.wrappers_pb2 import BoolValue
-from model_bakery import baker
 
 
 class FakeServicerContext:
@@ -71,93 +66,3 @@ class FakeServicerContext:
 @pytest.fixture
 def context():
     return FakeServicerContext()
-
-
-@pytest.fixture
-def user_id(mocker):
-    user_id = UUID("678574dd4a274d3cbfac10666b7613ef")
-
-    mock = mocker.AsyncMock(return_value=BoolValue(value=True))
-
-    mocker.patch(
-        "paper.services.PaperService.project.CheckUserJoinedProject",
-        new=mock,
-    )
-
-    return user_id
-
-
-@pytest.fixture
-def wrong_user_id(mocker):
-    user_id = UUID("678574dd4a274d3cbfac10666b7613ee")
-
-    mock = mocker.AsyncMock(return_value=BoolValue(value=False))
-
-    mocker.patch(
-        "paper.services.PaperService.project.CheckUserJoinedProject",
-        new=mock,
-    )
-
-    return user_id
-
-
-@pytest_asyncio.fixture(scope="function", autouse=True)
-async def clear_db(db):
-    yield
-    from paper.models import Paper
-
-    await Paper.objects.all().adelete()
-    assert await Paper.objects.all().acount() == 0
-
-
-@pytest.fixture
-def project_id():
-    return UUID("678574dd4a274d3cbfac10666b761300")
-
-
-@pytest_asyncio.fixture
-async def papers(db, project_id) -> list[UUID]:
-    from paper.models import Paper
-
-    await Paper.objects.filter(project_id=project_id).adelete()
-
-    papers = baker.prepare(
-        Paper,
-        project_id=project_id,
-        keywords=["test-keyword1", "test-keyword2", "test-keyword3"],
-        authors=["test-author1", "test-author2", "test-author3"],
-        tags=["test-tag1", "test-tag2", "test-tag3"],
-        publication_date=timezone.now(),
-        create_time=timezone.now(),
-        update_time=timezone.now(),
-        _quantity=10,
-    )
-
-    await Paper.objects.abulk_create(papers)
-
-    return [paper.id for paper in papers]
-
-
-@pytest.fixture
-def paper_detail():
-    from paperpilot_common.protobuf.common.util_pb2 import Date
-    from paperpilot_common.protobuf.paper.paper_pb2 import PaperDetail
-
-    return PaperDetail(
-        title="test",
-        abstract="test",
-        keywords=["test"],
-        authors=["test"],
-        tags=["test"],
-        publication_date=Date(
-            year=2021,
-            month=1,
-            day=1,
-        ),
-        publication="test",
-        volume="test",
-        issue="test",
-        pages="test",
-        doi="test",
-        url="test",
-    )
