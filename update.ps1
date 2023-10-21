53d23e0ebaba56843bf87c1bd9c1f1eba3c0be4d
Nagico <yjr888@vip.qq.com>
2023-10-21 21:52:53 +0800

    feat: 添加更新脚本

---
+++ update.ps1
@@ -0,0 +1,23 @@
+# 定义一个存储子仓库的列表
+$subtrees = @("coordinator", "file", "paper", "project", "reflection", "test", "translation", "user")
+
+# 遍历子仓库列表并执行 git subtree pull 命令
+foreach ($subtree in $subtrees) {
+    $prefix = "paperpilot-backend-$subtree"
+    $branch = "main"
+    
+    $command = "git subtree pull --prefix=$prefix $subtree $branch"
+    
+    Write-Host "run: $command"
+    
+    # 执行 git subtree pull 命令
+    Invoke-Expression $command
+}
+
+
+$command = "git pull"
+
+Write-Host "run: $command"
+
+# 执行 git pull
+Invoke-Expression $command
