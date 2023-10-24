1b5bcbdb6e7777e0e6e4cf9f7495b8df6fc30389
Nagico <yjr888@vip.qq.com>
2023-10-24 20:28:17 +0800

    feat: 添加health check反射

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import sys
 
 import grpc.aio
 import grpc_health.v1.health as health
-from grpc_health.v1 import health_pb2_grpc
+from grpc_health.v1 import health_pb2, health_pb2_grpc
 from grpc_reflection.v1alpha import reflection
 from paperpilot_common.utils.log import get_logger
 
@@ -22,13 +22,14 @@ async def serve(addr: str | None = None) -> None:
     service_names = []
     service_names.extend(monitor.grpc_hook(server))
 
-    # add reflection
-    service_names.append(reflection.SERVICE_NAME)
-    reflection.enable_server_reflection(service_names, server)
-
     # add health check
     health_servicer = health.aio.HealthServicer()
     health_pb2_grpc.add_HealthServicer_to_server(health_servicer, server)
+    service_names.append(health_pb2.DESCRIPTOR.services_by_name["Health"].full_name)
+
+    # add reflection
+    service_names.append(reflection.SERVICE_NAME)
+    reflection.enable_server_reflection(service_names, server)
 
     server.add_insecure_port(addr)
     logger.info(f"gRPC server listening on {addr}")
