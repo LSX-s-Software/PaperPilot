2e625898604b7e17bac40847f382a2a768d6d181
Nagico <yjr888@vip.qq.com>
2023-10-24 13:23:22 +0800

    feat: 添加healthcheck

--- server/urls.py
+++ server/urls.py
@@ -1,9 +1,11 @@
-import test.urls
-
-import callback.urls
 from starlette.routing import Mount
+from paperpilot_common.utils import health
+
+import test.urls
+import callback.urls
 
 routes = [
+    Mount("/health/", routes=health.routes),
     Mount("/file/test/", routes=test.urls.routes),
     Mount("/callback/", routes=callback.urls.routes),
 ]
