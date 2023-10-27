2ab4f608d41d950a50b3687ddf53665cb55a1a79
Nagico <yjr888@vip.qq.com>
2023-10-27 13:10:55 +0800

    fix: 修复设置导包路径

--- server/settings/__init__.py
+++ server/settings/__init__.py
@@ -1,4 +1,6 @@
 import os
+import sys
+from pathlib import Path
 
 from server.settings.components import *  # noqa
 from server.settings.util import _ENV
@@ -14,3 +16,9 @@ if os.path.exists(
     os.path.join(os.path.dirname(__file__), "environments", "local.py")
 ):
     from server.settings.environments.local import *  # noqa
+
+# Build paths inside the project like this: BASE_DIR.joinpath('some')
+# `pathlib` is better than writing: dirname(dirname(dirname(__file__)))
+BASE_DIR = Path(__file__).parent.parent.parent
+# 添加导包路径
+sys.path.insert(0, str(BASE_DIR.joinpath("server", "apps")))
