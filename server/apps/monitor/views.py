e86724cdadd3e39fc64789e79c540906850561ae
Nagico <yjr888@vip.qq.com>
2023-11-07 16:35:47 +0800

    fix: 修复图片被cache的问题

--- server/apps/monitor/views.py
+++ server/apps/monitor/views.py
@@ -6,8 +6,16 @@ from server.apps.monitor.services import monitor_service
 
 class StatusSvgView(HTTPEndpoint):
     async def get(self, request):
+        # no cache
+        headers = {
+            "Cache-Control": "no-cache, no-store, must-revalidate",
+            "Pragma": "no-cache",
+            "Expires": "0",
+        }
+
         return Response(
             status_code=200,
             media_type="image/svg+xml",
             content=await monitor_service.get_status_svg(),
+            headers=headers,
         )
